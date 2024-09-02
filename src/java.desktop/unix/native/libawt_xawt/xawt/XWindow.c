JetBrains/JetBrainsRuntime

JBR-3112 Linux: Last character issue with Korean.
A fix prototype.

src/java.desktop/unix/native/libawt_xawt/xawt/XWindow.c
@@ -86,7 +86,7 @@ jfieldID targetID;
 jfieldID graphicsConfigID;
 
 extern jobject currentX11InputMethodInstance;
-extern Boolean awt_x11inputmethod_lookupString(XKeyPressedEvent *, KeySym *);
+extern Boolean awt_x11inputmethod_lookupString(XKeyPressedEvent *, KeySym *, Boolean isKeyEventFirstAfterXResetIc);
 Boolean awt_UseType4Patch = False;
 Boolean awt_ServerDetected = False;
 Boolean awt_XKBDetected = False;
@@ -1103,7 +1103,7 @@ JNIEXPORT jboolean JNICALL Java_sun_awt_X11_XWindow_haveCurrentX11InputMethodIns
 }
 
 JNIEXPORT jboolean JNICALL Java_sun_awt_X11_XWindow_x11inputMethodLookupString
-(JNIEnv *env, jobject object, jlong event, jlongArray keysymArray) {
+(JNIEnv *env, jobject object, jlong event, jlongArray keysymArray, jboolean isKeyEventFirstAfterXResetIc) {
    KeySym keysym = NoSymbol;
    Boolean boo;
    /* keysymArray (and testbuf[]) have dimension 2 because we put there two
@@ -1117,7 +1117,11 @@ JNIEXPORT jboolean JNICALL Java_sun_awt_X11_XWindow_x11inputMethodLookupString
 
    testbuf[1]=0;
 
-   boo = awt_x11inputmethod_lookupString((XKeyPressedEvent*)jlong_to_ptr(event), &keysym);
+   boo = awt_x11inputmethod_lookupString(
+       (XKeyPressedEvent*)jlong_to_ptr(event),
+       &keysym,
+       (isKeyEventFirstAfterXResetIc == JNI_TRUE) ? True : False
+   );
    testbuf[0] = keysym;
 
    (*env)->SetLongArrayRegion(env, keysymArray, 0, 2, (jlong *)(testbuf));

arm32x/jbr-wayland-ssd

- Ignores the IM text returned from XmbLookupString/XwcLookupString if the KeyPress event which XmbResetIC was called with was synthetic and the first after a call of XmbResetIC/XwcResetIC.
- Only for the new mode introduced in JBR-2460 (-Djb.awt.newXimClient.preferBelowTheSpot=true): cancel text composing on each mouse press, so that preedit text stops following the caret if it's moving in response to mouse clicks.
(cherry picked from commit 43a9a3a17a2cfbe8078737e6e64f0cce6eb3cf8c)
(cherry picked from commit 156e5d9b65bbd996fe3e454dfce942a7f09c3249)

src/java.desktop/unix/native/libawt_xawt/xawt/XWindow.c
@@ -86,7 +86,7 @@ jfieldID targetID;
 jfieldID graphicsConfigID;
 
 extern jobject currentX11InputMethodInstance;
-extern Boolean awt_x11inputmethod_lookupString(XKeyPressedEvent *, KeySym *);
+extern Boolean awt_x11inputmethod_lookupString(XKeyPressedEvent *, KeySym *, Boolean keyPressContainsThePreeditTextOfLastXResetIC);
 Boolean awt_UseType4Patch = False;
 Boolean awt_ServerDetected = False;
 Boolean awt_XKBDetected = False;
@@ -1103,7 +1103,7 @@ JNIEXPORT jboolean JNICALL Java_sun_awt_X11_XWindow_haveCurrentX11InputMethodIns
 }
 
 JNIEXPORT jboolean JNICALL Java_sun_awt_X11_XWindow_x11inputMethodLookupString
-(JNIEnv *env, jobject object, jlong event, jlongArray keysymArray) {
+(JNIEnv *env, jobject object, jlong event, jlongArray keysymArray, jboolean keyPressContainsThePreeditTextOfLastXResetIC) {
    KeySym keysym = NoSymbol;
    Boolean boo;
    /* keysymArray (and testbuf[]) have dimension 2 because we put there two
@@ -1117,7 +1117,11 @@ JNIEXPORT jboolean JNICALL Java_sun_awt_X11_XWindow_x11inputMethodLookupString
 
    testbuf[1]=0;
 
-   boo = awt_x11inputmethod_lookupString((XKeyPressedEvent*)jlong_to_ptr(event), &keysym);
+   boo = awt_x11inputmethod_lookupString(
+       (XKeyPressedEvent*)jlong_to_ptr(event),
+       &keysym,
+       (keyPressContainsThePreeditTextOfLastXResetIC == JNI_TRUE) ? True : False
+   );
    testbuf[0] = keysym;
 
    (*env)->SetLongArrayRegion(env, keysymArray, 0, 2, (jlong *)(testbuf));