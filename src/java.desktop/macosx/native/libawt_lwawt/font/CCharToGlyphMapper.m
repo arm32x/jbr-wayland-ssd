yefengchun/JetBrainsRuntime

reimplement JDK-7162125 to fix JDK-8147002
port commit ba38e5c4 from JBR 9

src/java.desktop/macosx/native/libawt_lwawt/font/CCharToGlyphMapper.m
@@ -29,6 +29,7 @@
 #import "CoreTextSupport.h"
 
 #import "sun_font_CCharToGlyphMapper.h"
+#import "sun_font_CCompositeGlyphMapper.h"
 
 /*
  * Class:     sun_font_CCharToGlyphMapper
@@ -111,5 +112,30 @@ JNF_COCOA_ENTER(env);
                                               unicodesAsChars, JNI_ABORT);
     }
 
+JNF_COCOA_EXIT(env);
+}
+
+/*
+ * Class:     sun_font_CCompositeGlyphMapper
+ * Method:    nativeCodePointToGlyph
+ * Signature: (JI[Ljava/lang/String;)I
+ */
+JNIEXPORT jint JNICALL
+Java_sun_font_CCompositeGlyphMapper_nativeCodePointToGlyph
+(JNIEnv *env, jclass clazz, jlong awtFontPtr, jint codePoint, jobjectArray resultArray)
+{
+JNF_COCOA_ENTER(env);
+    AWTFont *awtFont = (AWTFont *)jlong_to_ptr(awtFontPtr);
+    CFStringRef fontNames[] = {NULL, NULL};
+    CGGlyph glyph = CTS_CopyGlyphAndFontNamesForCodePoint(awtFont, (UnicodeScalarValue)codePoint, fontNames);
+    if (glyph > 0) {
+        jstring fontName = (jstring)JNFNSToJavaString(env, (NSString *)fontNames[0]);
+        (*env)->SetObjectArrayElement(env, resultArray, 0, fontName);
+        jstring fontFamilyName = (jstring)JNFNSToJavaString(env, (NSString *)fontNames[1]);
+        (*env)->SetObjectArrayElement(env, resultArray, 1, fontFamilyName);
+    }
+    if (fontNames[0]) CFRelease(fontNames[0]);
+    if (fontNames[1]) CFRelease(fontNames[1]);
+    return glyph;
 JNF_COCOA_EXIT(env);
 }

arm32x/jbr-wayland-ssd

port from JBR 11 to JBR 15(cherry picked from commit a949f9d2201acefa77713bc21da4f971e95c898b)
cherry-picked from commit f309844f7580089efb024677a6a3eeef4cb8ed2b
with fix for JBR-5300 Change source code and test files to use GPL license

src/java.desktop/macosx/native/libawt_lwawt/font/CCharToGlyphMapper.m
@@ -29,6 +29,7 @@
 #import "CoreTextSupport.h"
 
 #import "sun_font_CCharToGlyphMapper.h"
+#import "sun_font_CCompositeGlyphMapper.h"
 
 /*
  * Class:     sun_font_CCharToGlyphMapper
@@ -113,5 +114,30 @@ JNI_COCOA_ENTER(env);
                                               unicodesAsChars, JNI_ABORT);
     }
 
+JNI_COCOA_EXIT(env);
+}
+
+/*
+ * Class:     sun_font_CCompositeGlyphMapper
+ * Method:    nativeCodePointToGlyph
+ * Signature: (JI[Ljava/lang/String;)I
+ */
+JNIEXPORT jint JNICALL
+Java_sun_font_CCompositeGlyphMapper_nativeCodePointToGlyph
+(JNIEnv *env, jclass clazz, jlong awtFontPtr, jint codePoint, jobjectArray resultArray)
+{
+JNI_COCOA_ENTER(env);
+    AWTFont *awtFont = (AWTFont *)jlong_to_ptr(awtFontPtr);
+    CFStringRef fontNames[] = {NULL, NULL};
+    CGGlyph glyph = CTS_CopyGlyphAndFontNamesForCodePoint(awtFont, (UnicodeScalarValue)codePoint, fontNames);
+    if (glyph > 0) {
+        jstring fontName = NSStringToJavaString(env, (NSString *)fontNames[0]);
+        (*env)->SetObjectArrayElement(env, resultArray, 0, fontName);
+        jstring fontFamilyName = NSStringToJavaString(env, (NSString *)fontNames[1]);
+        (*env)->SetObjectArrayElement(env, resultArray, 1, fontFamilyName);
+    }
+    if (fontNames[0]) CFRelease(fontNames[0]);
+    if (fontNames[1]) CFRelease(fontNames[1]);
+    return glyph;
 JNI_COCOA_EXIT(env);
 }