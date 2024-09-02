2lambda123/JetBrainsRuntime

JBR-5661 Wayland: implement heavy-weight popup windows

src/java.desktop/share/classes/sun/awt/AWTAccessor.java
@@ -310,6 +310,9 @@ public final class AWTAccessor {
 
         boolean needUpdateWindow(Window window);
 
+        void setPopupTarget(Window window, Component component);
+        Component getPopupTarget(Window window);
+
         /**
          * Set the size of the security warning.
          */

arm32x/jbr-wayland-ssd

src/java.desktop/share/classes/sun/awt/AWTAccessor.java
@@ -311,6 +311,9 @@ public final class AWTAccessor {
 
         boolean needUpdateWindow(Window window);
 
+        void setPopupParent(Window window, Component component);
+        Component getPopupParent(Window window);
+
         /**
          * Set the size of the security warning.
          */