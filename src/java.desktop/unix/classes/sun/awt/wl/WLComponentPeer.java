openjdk/wakefield

Wayland: use server-side decorations if possible

src/java.desktop/unix/classes/sun/awt/wl/WLComponentPeer.java
@@ -328,7 +328,8 @@ public class WLComponentPeer implements ComponentPeer {
                             getParentNativePtr(target),
                             xNative, yNative,
                             isModal, isMaximized, isMinimized,
-                            title, WLToolkit.getApplicationID());
+                            title, WLToolkit.getApplicationID(),
+                            isUndecorated());
                 }
                 final long wlSurfacePtr = getWLSurface(nativePtr);
                 WLToolkit.registerWLSurface(wlSurfacePtr, this);
@@ -345,6 +346,10 @@ public class WLComponentPeer implements ComponentPeer {
                 nativeHideFrame(nativePtr);
             });
         }
+    }
+
+    protected boolean isUndecorated() {
+        return true;
     }
 
     /**
@@ -1023,7 +1028,7 @@ public class WLComponentPeer implements ComponentPeer {
 
     protected native void nativeCreateWLSurface(long ptr, long parentPtr,
                                                 int x, int y, boolean isModal, boolean isMaximized, boolean isMinimized,
-                                                String title, String appID);
+                                                String title, String appID, boolean wantsClientSideDecoration);
 
     protected native void nativeCreateWLPopup(long ptr, long parentPtr,
                                               int width, int height,
@@ -1225,7 +1230,7 @@ public class WLComponentPeer implements ComponentPeer {
         }
     }
 
-    void notifyConfigured(int newXNative, int newYNative, int newWidthNative, int newHeightNative, boolean active, boolean maximized) {
+    void notifyConfigured(int newXNative, int newYNative, int newWidthNative, int newHeightNative, boolean active, boolean maximized, boolean serverSideDecorations) {
         int newWidth = surfaceUnitsToJavaUnits(newWidthNative);
         int newHeight = surfaceUnitsToJavaUnits(newHeightNative);
         final long wlSurfacePtr = getWLSurface(nativePtr);

src/java.desktop/unix/classes/sun/awt/wl/WLDecoratedPeer.java
@@ -110,9 +110,15 @@ public abstract class WLDecoratedPeer extends WLWindowPeer {
     }
 
     @Override
-    void notifyConfigured(int newX, int newY, int newWidthNative, int newHeightNative, boolean active, boolean maximized) {
-        super.notifyConfigured(newX, newY, newWidthNative, newHeightNative, active, maximized);
+    void notifyConfigured(int newX, int newY, int newWidthNative, int newHeightNative, boolean active, boolean maximized, boolean serverSideDecorations) {
+        super.notifyConfigured(newX, newY, newWidthNative, newHeightNative, active, maximized, serverSideDecorations);
+        decoration.setHasServerSideDecoration(serverSideDecorations);
         decoration.setActive(active);
+    }
+
+    @Override
+    protected boolean isUndecorated() {
+        return decoration.isUndecorated();
     }
 
     @Override

src/java.desktop/unix/classes/sun/awt/wl/WLFramePeer.java
@@ -151,11 +151,11 @@ public class WLFramePeer extends WLDecoratedPeer implements FramePeer {
     }
 
     @Override
-    void notifyConfigured(int newXNative, int newYNative, int newWidthNative, int newHeightNative, boolean active, boolean maximized) {
+    void notifyConfigured(int newXNative, int newYNative, int newWidthNative, int newHeightNative, boolean active, boolean maximized, boolean serverSideDecorations) {
         int widthBefore = getWidth();
         int heightBefore = getHeight();
 
-        super.notifyConfigured(newXNative, newYNative, newWidthNative, newHeightNative, active, maximized);
+        super.notifyConfigured(newXNative, newYNative, newWidthNative, newHeightNative, active, maximized, serverSideDecorations);
 
         synchronized (getStateLock()) {
             int oldState = state;

arm32x/jbr-wayland-ssd

src/java.desktop/unix/classes/sun/awt/wl/WLComponentPeer.java
@@ -359,7 +359,8 @@ public class WLComponentPeer implements ComponentPeer {
                             getParentNativePtr(target),
                             xNative, yNative,
                             isModal, isMaximized, isMinimized,
-                            title, WLToolkit.getApplicationID());
+                            title, WLToolkit.getApplicationID(),
+                            isUndecorated());
                 }
                 final long wlSurfacePtr = getWLSurface(nativePtr);
                 WLToolkit.registerWLSurface(wlSurfacePtr, this);
@@ -376,6 +377,10 @@ public class WLComponentPeer implements ComponentPeer {
                 nativeHideFrame(nativePtr);
             });
         }
+    }
+
+    protected boolean isUndecorated() {
+        return true;
     }
 
     /**
@@ -1029,7 +1034,7 @@ public class WLComponentPeer implements ComponentPeer {
 
     protected native void nativeCreateWLSurface(long ptr, long parentPtr,
                                                 int x, int y, boolean isModal, boolean isMaximized, boolean isMinimized,
-                                                String title, String appID);
+                                                String title, String appID, boolean wantsClientSideDecoration);
 
     protected native void nativeCreateWLPopup(long ptr, long parentPtr,
                                               int width, int height,
@@ -1237,7 +1242,7 @@ public class WLComponentPeer implements ComponentPeer {
         return new Dimension(javaUnitsToSurfaceUnits(d.width), javaUnitsToSurfaceUnits(d.height));
     }
 
-    void notifyConfigured(int newSurfaceX, int newSurfaceY, int newSurfaceWidth, int newSurfaceHeight, boolean active, boolean maximized) {
+    void notifyConfigured(int newSurfaceX, int newSurfaceY, int newSurfaceWidth, int newSurfaceHeight, boolean active, boolean maximized, boolean serverSideDecorations) {
         // NB: The width and height, as well as X and Y arguments specify the size and the location
         //     of the window in surface-local coordinates.
         if (log.isLoggable(PlatformLogger.Level.FINE)) {

src/java.desktop/unix/classes/sun/awt/wl/WLDecoratedPeer.java
@@ -110,8 +110,9 @@ public abstract class WLDecoratedPeer extends WLWindowPeer {
     }
 
     @Override
-    void notifyConfigured(int newSurfaceX, int newSurfaceY, int newSurfaceWidth, int newSurfaceHeight, boolean active, boolean maximized) {
-        super.notifyConfigured(newSurfaceX, newSurfaceY, newSurfaceWidth, newSurfaceHeight, active, maximized);
+    void notifyConfigured(int newSurfaceX, int newSurfaceY, int newSurfaceWidth, int newSurfaceHeight, boolean active, boolean maximized, boolean serverSideDecorations) {
+        super.notifyConfigured(newSurfaceX, newSurfaceY, newSurfaceWidth, newSurfaceHeight, active, maximized, serverSideDecorations);
+        decoration.setHasServerSideDecoration(serverSideDecorations);
         decoration.setActive(active);
     }
 

src/java.desktop/unix/classes/sun/awt/wl/WLFramePeer.java
@@ -165,11 +165,11 @@ public class WLFramePeer extends WLDecoratedPeer implements FramePeer {
     }
 
     @Override
-    void notifyConfigured(int newSurfaceX, int newSurfaceY, int newSurfaceWidth, int newSurfaceHeight, boolean active, boolean maximized) {
+    void notifyConfigured(int newSurfaceX, int newSurfaceY, int newSurfaceWidth, int newSurfaceHeight, boolean active, boolean maximized, boolean serverSideDecorations) {
         int widthBefore = getWidth();
         int heightBefore = getHeight();
 
-        super.notifyConfigured(newSurfaceX, newSurfaceY, newSurfaceWidth, newSurfaceHeight, active, maximized);
+        super.notifyConfigured(newSurfaceX, newSurfaceY, newSurfaceWidth, newSurfaceHeight, active, maximized, serverSideDecorations);
 
         synchronized (getStateLock()) {
             int oldState = state;