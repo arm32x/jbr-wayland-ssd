JetBrains/JetBrainsRuntime

Added JBR-specific .idea project files

bin/idea.sh
@@ -114,6 +114,7 @@ cd "$SCRIPT_DIR/.." ; OPENJDK_DIR=`pwd`
 IDEA_MAKE="$OPENJDK_DIR/make/ide/idea/jdk"
 IDEA_TEMPLATE="$IDEA_MAKE/template"
 
+cp -rn "$TOPLEVEL_DIR/jb/project/idea-project-files"/* "$IDEA_OUTPUT"
 cp -rn "$IDEA_TEMPLATE"/* "$IDEA_OUTPUT"
 
 #override template

arm32x/jbr-wayland-ssd

bin/idea.sh
@@ -85,6 +85,7 @@ MAKE_DIR="$SCRIPT_DIR/../make"
 IDEA_MAKE="$MAKE_DIR/ide/idea/jdk"
 IDEA_TEMPLATE="$IDEA_MAKE/template"
 
+cp -r "$TOPLEVEL_DIR/jb/project/idea-project-files"/* "$IDEA_OUTPUT"
 cp -r "$IDEA_TEMPLATE"/* "$IDEA_OUTPUT"
 
 #override template